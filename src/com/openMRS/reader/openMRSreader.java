88d682f8a06d992482cff19fd14fc3067e98b2b8
Mark Klein <markpklein@gmail.com>
2013-01-21 10:50:41 -1000

    Fix bugs, add splash and add popup

--- src/com/openMRS/reader/openMRSreader.java
+++ src/com/openMRS/reader/openMRSreader.java
@@ -29,7 +29,8 @@ public class openMRSreader extends DroidGap
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
+        super.loadUrl("file:///android_asset/www/index.html", 5000);
     }
 }
 
